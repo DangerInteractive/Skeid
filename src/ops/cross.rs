56810f9ad30b2f2f6e50bbf0c719f6a8053dc2eb
Alexander Barber <alex@dangerzonegames.com>
2022-11-20 15:55:39 -0700

    add documentation for Dot, DotAssign, Cross, and CrossAssign traits

--- src/ops/cross.rs
+++ src/ops/cross.rs
@@ -1,9 +1,14 @@
+/// The cross product operator (for vector/matrix math)
 pub trait Cross<Rhs = Self> {
+    /// The resulting type after applying the cross product operator
     type Output;
 
+    /// Performs the cross product operation
     fn cross(self, rhs: Rhs) -> Self::Output;
 }
 
+/// The in-place cross product operator (for vector/matrix math)
 pub trait CrossAssign<Rhs = Self> {
+    /// Performs the cross product operation in place (mutates self)
     fn cross_assign(&mut self, rhs: Rhs);
 }
--- src/ops/dot.rs
+++ src/ops/dot.rs
@@ -1,9 +1,14 @@
+/// The dot product operator (for vector/matrix math)
 pub trait Dot<Rhs = Self> {
+    /// The resulting type after applying the dot product operator
     type Output;
 
+    /// Performs the dot product operation
     fn dot(self, rhs: Rhs) -> Self::Output;
 }
 
+/// The in-place dot product operator (for vector/matrix math)
 pub trait DotAssign<Rhs = Self> {
+    /// Performs the dot product operation in place (mutates self)
     fn dot_assign(&mut self, rhs: Rhs);
 }
