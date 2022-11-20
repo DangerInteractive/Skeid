38e8026d40d4e83d5c7e66c90227e0770b6c6265
Alexander Barber <alex@dangerzonegames.com>
2022-11-20 14:14:48 -0700

    Revert "removing mul/div where both sides are vectors"
    This reverts commit f27602331bfd7d2c8cf7774ccf87e4bcd20fa90c.

--- src/vector/multiply.rs
+++ src/vector/multiply.rs
@@ -1,6 +1,18 @@
+use std::ops::{Mul, MulAssign};
 use crate::marker::Scalar;
 use crate::vector::Vector;
-use std::ops::{Mul, MulAssign};
+
+impl<T, const S: usize, By> Mul<Vector<By, S>> for Vector<T, S>
+where
+    T: Sized + Copy + Mul<By>,
+    By: Sized + Copy,
+{
+    type Output = Vector<<T as Mul<By>>::Output, S>;
+
+    fn mul(self, rhs: Vector<By, S>) -> Self::Output {
+        self.into_vector_op(rhs, move |lhs_value, rhs_value| lhs_value * rhs_value)
+    }
+}
 
 impl<T, const S: usize, By> Mul<By> for Vector<T, S>
 where
@@ -12,6 +24,16 @@ where
 
     fn mul(self, rhs: By) -> Self::Output {
         self.into_scalar_op(rhs, move |lhs_value, rhs_value| lhs_value * rhs_value)
+    }
+}
+
+impl<T, const S: usize, By> MulAssign<Vector<By, S>> for Vector<T, S>
+where
+    T: Sized + Copy + Mul<By, Output = T>,
+    By: Sized + Copy,
+{
+    fn mul_assign(&mut self, rhs: Vector<By, S>) {
+        self.assign_from_vector_op(rhs, move |lhs_value, rhs_value| lhs_value * rhs_value)
     }
 }
 
