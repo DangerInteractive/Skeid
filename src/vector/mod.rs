c24dbf836f87444da54ab41a2d93903e26692f2e
Alexander Barber <alex@dangerzonegames.com>
2022-11-20 00:57:50 -0700

    separate function for magnitude squared, into/assign normalized vectors

--- src/vector/mod.rs
+++ src/vector/mod.rs
@@ -1,5 +1,5 @@
 use std::array::from_fn;
-use std::ops::Div;
+use std::ops::{Div, DivAssign};
 
 mod add;
 mod divide;
@@ -16,7 +16,7 @@ pub type Vector3<T> = Vector<T, 3>;
 pub type Vector4<T> = Vector<T, 4>;
 
 impl<T: Sized + Copy, const S: usize> Vector<T, S> {
-    pub fn magnitude_f64(&self) -> f64
+    pub fn magnitude_squared_f64(&self) -> f64
     where
         T: Into<f64>,
     {
@@ -25,10 +25,10 @@ impl<T: Sized + Copy, const S: usize> Vector<T, S> {
             let x = self.array[i].into();
             sum += x.powi(2);
         }
-        sum.sqrt()
+        sum
     }
 
-    pub fn magnitude_f32(&self) -> f32
+    pub fn magnitude_squared_f32(&self) -> f32
     where
         T: Into<f32>,
     {
@@ -37,7 +37,21 @@ impl<T: Sized + Copy, const S: usize> Vector<T, S> {
             let x = self.array[i].into();
             sum += x.powi(2)
         }
-        sum.sqrt()
+        sum
+    }
+
+    pub fn magnitude_f64(&self) -> f64
+    where
+        T: Into<f64>,
+    {
+        self.magnitude_squared_f64().sqrt()
+    }
+
+    pub fn magnitude_f32(&self) -> f32
+    where
+        T: Into<f32>,
+    {
+        self.magnitude_squared_f32().sqrt()
     }
 
     pub fn normalize_f64(&self) -> Self
@@ -54,6 +68,38 @@ impl<T: Sized + Copy, const S: usize> Vector<T, S> {
         Vector<T, S>: Div<f32, Output = Vector<T, S>>,
     {
         *self / self.magnitude_f32()
+    }
+
+    pub fn into_normalized_f64(self) -> Self
+    where
+        T: Div<f64> + Into<f64>,
+        Vector<T, S>: Div<f64, Output = Vector<T, S>>,
+    {
+        self / self.magnitude_f64()
+    }
+
+    pub fn into_normalized_f32(self) -> Self
+    where
+        T: Div<f32> + Into<f32>,
+        Vector<T, S>: Div<f32, Output = Vector<T, S>>,
+    {
+        self / self.magnitude_f32()
+    }
+
+    pub fn assign_normalized_f64(&mut self)
+    where
+        T: Div<f64> + Into<f64>,
+        Vector<T, S>: DivAssign<f64>,
+    {
+        *self /= self.magnitude_f64();
+    }
+
+    pub fn assign_normalized_f32(&mut self)
+    where
+        T: Div<f32> + Into<f32>,
+        Vector<T, S>: DivAssign<f32>,
+    {
+        *self /= self.magnitude_f32();
     }
 
     #[inline]
