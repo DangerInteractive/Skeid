506485a8e1d6029c7c8fe2b4a61ee7e86888ea4e
Alexander Barber <alex@dangerzonegames.com>
2022-11-20 01:06:06 -0700

    remove add/sub where left is vector, right is scalar

--- src/vector/add.rs
+++ src/vector/add.rs
@@ -1,6 +1,5 @@
-use std::ops::{Add, AddAssign};
-use crate::marker::Scalar;
 use crate::vector::Vector;
+use std::ops::{Add, AddAssign};
 
 impl<T, const S: usize, By> Add<Vector<By, S>> for Vector<T, S>
 where
@@ -16,18 +15,6 @@ where
     }
 }
 
-impl<T, const S: usize, By> Add<By> for Vector<T, S>
-where
-    T: Sized + Copy + Add<By>,
-    By: Scalar + Sized + Copy,
-{
-    type Output = Vector<<T as Add<By>>::Output, S>;
-
-    fn add(self, rhs: By) -> Self::Output {
-        self.into_scalar_op(rhs, move |lhs_value, rhs_value| lhs_value + rhs_value)
-    }
-}
-
 impl<T, const S: usize, By> AddAssign<Vector<By, S>> for Vector<T, S>
 where
     T: Sized + Copy + Add<By, Output = T>,
@@ -35,15 +22,5 @@ where
 {
     fn add_assign(&mut self, rhs: Vector<By, S>) {
         self.assign_from_vector_op::<By>(rhs, move |lhs_value, rhs_value| lhs_value + rhs_value)
-    }
-}
-
-impl<T, const S: usize, By> AddAssign<By> for Vector<T, S>
-where
-    T: Sized + Copy + Add<By, Output = T>,
-    By: Scalar + Sized + Copy,
-{
-    fn add_assign(&mut self, rhs: By) {
-        self.assign_from_scalar_op::<By>(rhs, move |lhs_value, rhs_value| lhs_value + rhs_value)
     }
 }
--- src/vector/subtract.rs
+++ src/vector/subtract.rs
@@ -1,6 +1,5 @@
-use std::ops::{Sub, SubAssign};
-use crate::marker::Scalar;
 use crate::vector::Vector;
+use std::ops::{Sub, SubAssign};
 
 impl<T, const S: usize, By> Sub<Vector<By, S>> for Vector<T, S>
 where
@@ -16,18 +15,6 @@ where
     }
 }
 
-impl<T, const S: usize, By> Sub<By> for Vector<T, S>
-where
-    T: Sized + Copy + Sub<By>,
-    By: Scalar + Sized + Copy,
-{
-    type Output = Vector<<T as Sub<By>>::Output, S>;
-
-    fn sub(self, rhs: By) -> Self::Output {
-        self.into_scalar_op(rhs, move |lhs_value, rhs_value| lhs_value - rhs_value)
-    }
-}
-
 impl<T, const S: usize, By> SubAssign<Vector<By, S>> for Vector<T, S>
 where
     T: Sized + Copy + Sub<By, Output = T>,
@@ -35,15 +22,5 @@ where
 {
     fn sub_assign(&mut self, rhs: Vector<By, S>) {
         self.assign_from_vector_op::<By>(rhs, move |lhs_value, rhs_value| lhs_value - rhs_value)
-    }
-}
-
-impl<T, const S: usize, By> SubAssign<By> for Vector<T, S>
-where
-    T: Sized + Copy + Sub<By, Output = T>,
-    By: Scalar + Sized + Copy,
-{
-    fn sub_assign(&mut self, rhs: By) {
-        self.assign_from_scalar_op::<By>(rhs, move |lhs_value, rhs_value| lhs_value - rhs_value)
     }
 }
