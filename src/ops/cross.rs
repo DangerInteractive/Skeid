29e07ea284cafabd642c9313db5a17102872ab0e
Alexander Barber <alex@dangerzonegames.com>
2023-01-02 21:15:31 -0700

    remove DotAssign (returns different type) and CrossAssign (values depend on other indices and cannot be calculated in place)

--- src/ops/cross.rs
+++ src/ops/cross.rs
@@ -6,9 +6,3 @@ pub trait Cross<Rhs = Self> {
     /// Performs the cross product operation
     fn cross(self, rhs: Rhs) -> Self::Output;
 }
-
-/// The in-place cross product operator (for vector/matrix math)
-pub trait CrossAssign<Rhs = Self> {
-    /// Performs the cross product operation in place (mutates self)
-    fn cross_assign(&mut self, rhs: Rhs);
-}
--- src/ops/dot.rs
+++ src/ops/dot.rs
@@ -6,9 +6,3 @@ pub trait Dot<Rhs = Self> {
     /// Performs the dot product operation
     fn dot(self, rhs: Rhs) -> Self::Output;
 }
-
-/// The in-place dot product operator (for vector/matrix math)
-pub trait DotAssign<Rhs = Self> {
-    /// Performs the dot product operation in place (mutates self)
-    fn dot_assign(&mut self, rhs: Rhs);
-}
