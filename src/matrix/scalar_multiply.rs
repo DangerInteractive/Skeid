7f1d156ef9baea5114510c682fd20482e201b7db
Alex Barber <alex@dangerzonegames.com>
2023-04-11 21:49:02 -0600

    add known product test to Matrix-Scalar multiplication

--- src/matrix/scalar_multiply.rs
+++ src/matrix/scalar_multiply.rs
@@ -27,6 +27,20 @@ where
 }
 
 #[test]
+fn known_product() {
+    let matrix_a = Matrix::<i32, 3, 3>::from_array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]);
+    let scalar_t = 123;
+    let product =
+        Matrix::<i32, 3, 3>::from_array([[123, 246, 369], [492, 615, 738], [861, 984, 1107]]);
+
+    assert_eq!(
+        matrix_a * scalar_t,
+        product,
+        "Matrix-Scalar multiplication matches known product"
+    )
+}
+
+#[test]
 fn associative_law() {
     let scalar_s = 123;
     let scalar_t = 456;
@@ -58,6 +72,6 @@ fn distributive_law() {
     assert_eq!(
         matrix_a * (scalar_s + scalar_t),
         (matrix_a * scalar_s) + (matrix_a * scalar_t),
-        "Matrix-Scalar multiplication obeys the distributive law variation 2: A(s+t) = As + At"
+        "Matrix-Scalar multiplication obeys the distributive law variation 2: A(s+t) = As+At"
     )
 }
