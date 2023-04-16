dba731908654461d80d0f85aaa4989af539a7dcf
Alex Barber <alex@dangerzonegames.com>
2023-04-15 23:47:10 -0600

    add known sum tests to matrix-matrix addition

--- src/matrix/add.rs
+++ src/matrix/add.rs
@@ -30,6 +30,38 @@ where
 }
 
 #[test]
+fn known_sum() {
+    let matrix_a = Matrix::<i32, 3, 3>::from_array([[-13, 65, 62], [41, -108, -93], [91, 19, -80]]);
+    let matrix_b = Matrix::<i32, 3, 3>::from_array([[-22, 3, 59], [-26, 84, -16], [-77, 106, -44]]);
+    let matrix_c = Matrix::<i32, 3, 3>::from_array([[106, 73, 3], [76, 86, -2], [-93, -117, -110]]);
+
+    let sum_1 = matrix_a + matrix_b;
+    let expected_sum_1 =
+        Matrix::<i32, 3, 3>::from_array([[-35, 68, 121], [15, -24, -109], [14, 125, -124]]);
+
+    let sum_2 = matrix_b + matrix_c;
+    let expected_sum_2 =
+        Matrix::<i32, 3, 3>::from_array([[84, 76, 62], [50, 170, -18], [-170, -11, -154]]);
+
+    let sum_3 = matrix_c + matrix_a;
+    let expected_sum_3 =
+        Matrix::<i32, 3, 3>::from_array([[93, 138, 65], [117, -22, -95], [-2, -98, -190]]);
+
+    assert_eq!(
+        sum_1, expected_sum_1,
+        "Matrix-Matrix addition matches known sum 1"
+    );
+    assert_eq!(
+        sum_2, expected_sum_2,
+        "Matrix-Matrix addition matches known sum 2"
+    );
+    assert_eq!(
+        sum_3, expected_sum_3,
+        "Matrix-Matrix addition matches known sum 3"
+    );
+}
+
+#[test]
 fn associative_law() {
     let matrix_a = Matrix::<i32, 3, 3>::from_array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]);
     let matrix_b = Matrix::<i32, 3, 3>::from_array([[10, 11, 12], [13, 14, 15], [16, 17, 18]]);
